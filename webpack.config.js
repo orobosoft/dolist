7c273d0d3f227552cdb729349c34be0145cdc293
Orobosa <95650588+orobosaik@users.noreply.github.com>
2022-07-26 15:53:36 +0100

    Add npm and webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+// process.env.NODE_ENV = "production";
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+	mode: "development",
+	entry: {
+		index: "./src/index.js",
+	},
+	devtool: "source-map",
+	// devServer: {
+	// 	static: "./dist",
+	// },
+	output: {
+		filename: "main.js",
+		path: path.resolve(__dirname, "dist"),
+		clean: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: [MiniCssExtractPlugin.loader, "css-loader"],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: "asset/resource",
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: "asset/resource",
+			},
+		],
+	},
+	optimization: {
+		minimize: true,
+		minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({ template: "./src/index.html", title: "DoList" }),
+		new MiniCssExtractPlugin({ filename: "[name].css" }),
+	],
+	performance: {
+		hints: process.env.NODE_ENV === "production" ? "warning" : false,
+	},
+};
